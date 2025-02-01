8bafb0541c62e1c0e508e3dbc4e1157fdb5ee327
Cullen Walsh <ckwalsh@cullenwalsh.com>
2025-01-31 17:44:02 -0800

    Export GENERATED_TAG and PARTIALLY_GENERATED_TAG

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@
 export { default as signCode } from './sign';
 export { default as verifyCode } from './verify';
 export {
+  GENERATED_TAG,
   GENERATED_TOKEN,
+  PARTIALLY_GENERATED_TAG,
   PARTIALLY_GENERATED_TOKEN,
   BEGIN_MANUAL_SECTION_TOKEN,
   END_MANUAL_SECTION_TOKEN,
